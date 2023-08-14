90f9434b92622eb4a25ae29d499c37f55e359fb2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-14 15:44:45 +0000

    another test :D

--- lib/components/home_components/scanned_item/scanned_item_widget.dart
+++ lib/components/home_components/scanned_item/scanned_item_widget.dart
@@ -4,6 +4,7 @@ import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/custom_code/actions/index.dart' as actions;
 import '/custom_code/widgets/index.dart' as custom_widgets;
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -115,7 +116,7 @@ class _ScannedItemWidgetState extends State<ScannedItemWidget> {
                                 ),
                               ),
                             ),
-                            Text(
+                            AutoSizeText(
                               valueOrDefault<String>(
                                 dateTimeFormat(
                                   'EEEE, H:mm',
