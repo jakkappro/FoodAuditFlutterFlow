58d2fce494c99d97478d6c52ea64ba51a4968dfd
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-29 19:35:42 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/index.dart
+++ lib/custom_code/widgets/index.dart
@@ -8,3 +8,4 @@ export 'safety_dot_stateless_wrapper.dart' show SafetyDotStatelessWrapper;
 export 'scanned_item_safety_text.dart' show ScannedItemSafetyText;
 export 'image_from_string.dart' show ImageFromString;
 export 'medication_display.dart' show MedicationDisplay;
+export 'o_c_r_scanning.dart' show OCRScanning;
---
+++ lib/custom_code/widgets/o_c_r_scanning.dart
@@ -0,0 +1,32 @@
+// Automatic FlutterFlow imports
+import '/backend/backend.dart';
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import '/custom_code/actions/index.dart'; // Imports custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class OCRScanning extends StatefulWidget {
+  const OCRScanning({
+    Key? key,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+
+  @override
+  _OCRScanningState createState() => _OCRScanningState();
+}
+
+class _OCRScanningState extends State<OCRScanning> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
