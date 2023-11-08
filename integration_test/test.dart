49ceff0e7494d69e019d2e9216585e086b2e9539
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-08 16:31:55 +0000

    latest just to be sure

---
+++ integration_test/test.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:nu3_food/main.dart';
+import 'package:nu3_food/flutter_flow/flutter_flow_util.dart';
+
+import 'package:provider/provider.dart';
+import 'package:nu3_food/backend/firebase/firebase_config.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Test test', (WidgetTester tester) async {
+    _overrideOnError();
+    await initFirebase();
+    await FirebaseAuth.instance.signOut();
+    FFAppState.reset();
+    final appState = FFAppState();
+    await appState.initializePersistedState();
+
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => appState,
+      child: MyApp(),
+    ));
+
+    await tester.tap(find.text('Continue without registration'));
+    await tester.pumpAndSettle(
+      Duration(milliseconds: 10),
+      EnginePhase.sendSemanticsUpdate,
+      Duration(milliseconds: 3000),
+    );
+    expect(find.text('Hi user!'), findsWidgets);
+  });
+
+  testWidgets('Test of auth', (WidgetTester tester) async {
+    _overrideOnError();
+    await initFirebase();
+    await FirebaseAuth.instance.signOut();
+    FFAppState.reset();
+    final appState = FFAppState();
+    await appState.initializePersistedState();
+
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: 'kapitulcinjakub1@gmail.com', password: 'heslo123');
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => appState,
+      child: MyApp(),
+    ));
+
+    await tester.tap(find.text('Sign in with Google'));
+    await tester.pumpAndSettle(Duration(milliseconds: 20));
+    expect(find.text('Create your profile'), findsOneWidget);
+  });
+
+  testWidgets('LoggedOutSettingsTest', (WidgetTester tester) async {
+    _overrideOnError();
+    await initFirebase();
+    await FirebaseAuth.instance.signOut();
+    FFAppState.reset();
+    final appState = FFAppState();
+    await appState.initializePersistedState();
+
+    await tester.pumpWidget(ChangeNotifierProvider(
+      create: (context) => appState,
+      child: MyApp(),
+    ));
+
+    await tester.tap(find.byKey(ValueKey('Home_h7zf')));
+  });
+}
+
+// There are certain types of errors that can happen during tests but
+// should not break the test.
+void _overrideOnError() {
+  final originalOnError = FlutterError.onError!;
+  FlutterError.onError = (errorDetails) {
+    if (_shouldIgnoreError(errorDetails.toString())) {
+      return;
+    }
+    originalOnError(errorDetails);
+  };
+}
+
+bool _shouldIgnoreError(String error) {
+  // It can fail to decode some SVGs - this should not break the test.
+  if (error.contains('ImageCodecException')) {
+    return true;
+  }
+  // Overflows happen all over the place,
+  // but they should not break tests.
+  if (error.contains('overflowed by')) {
+    return true;
+  }
+  // Sometimes some images fail to load, it generally does not break the test.
+  if (error.contains('No host specified in URI') ||
+      error.contains('EXCEPTION CAUGHT BY IMAGE RESOURCE SERVICE')) {
+    return true;
+  }
+  // These errors should be avoided, but they should not break the test.
+  if (error.contains('setState() called after dispose()')) {
+    return true;
+  }
+
+  return false;
+}
--- lib/custom_code/widgets/o_c_r_scanning.dart
+++ lib/custom_code/widgets/o_c_r_scanning.dart
@@ -10,6 +10,23 @@ import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+// my imports
+import 'package:image/image.dart' as img;
+import '../../components/scanner_page_components/close_scanner_button/close_scanner_button_widget.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_mlkit_barcode_scanning/google_mlkit_barcode_scanning.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:nu3_food/ocr_components/o_c_r_header/o_c_r_header_widget.dart';
+
+enum states {
+  SCANNING_NAME,
+  SCANNING_EAN,
+  SCANNING_NUTRITION,
+  SCANNING_INGREDIENTS,
+  SCANNING_ALLERGENS,
+}
+
 class OCRScanning extends StatefulWidget {
   const OCRScanning({
     Key? key,
@@ -25,8 +42,294 @@ class OCRScanning extends StatefulWidget {
 }
 
 class _OCRScanningState extends State<OCRScanning> {
+  // colors
+  final Color _neutralColor = Color(0x1C0D26).withOpacity(0.5);
+  late Color _backdropColor; //initial color
+
+  late states currentState;
+
+  Map<states, String> messages = {
+    states.SCANNING_NAME: "Scanning name...",
+    states.SCANNING_EAN: "Scanning EAN...",
+    states.SCANNING_NUTRITION: "Scanning nutrition...",
+    states.SCANNING_INGREDIENTS: "Scanning ingredients...",
+    states.SCANNING_ALLERGENS: "Scanning allergens...",
+  };
+  Map<states, Size> sizes = {
+    states.SCANNING_NAME: Size(0.5, 0.1),
+    states.SCANNING_EAN: Size(0.5, 0.1),
+    states.SCANNING_NUTRITION: Size(0.5, 0.1),
+    states.SCANNING_INGREDIENTS: Size(0.5, 0.1),
+    states.SCANNING_ALLERGENS: Size(0.5, 0.1),
+  };
+
+  late Size currentSize;
+  late String currentMessage;
+
+  bool isTorchOn = false;
+
+  @override
+  void initState() {
+    _backdropColor = _neutralColor;
+    currentMessage = messages[currentState]!;
+    currentMessage = messages[currentState]!;
+    currentSize = sizes[currentState]!;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: <Widget>[
+        _ScannerPage(
+          onEanScanned: _onDataScanned,
+          isTorchOn: isTorchOn,
+          scanningType: currentState == states.SCANNING_EAN ? 1 : 0,
+        ),
+        ClipPath(
+          clipper: HoleClipper(currentSize.width, currentSize.height),
+          child: Container(
+            color: _backdropColor,
+          ),
+        ),
+        Align(
+          child: CloseScannerButtonWidget(),
+          alignment: Alignment(0.9, 0.9),
+        ),
+        SizedBox(
+          child: OCRHeaderWidget(
+            currentName: currentMessage,
+          ),
+          width: double.infinity,
+          height: 204,
+        ),
+        Align(
+          child: _switchTorchToggle(),
+          alignment: Alignment(-0.9, 0.9),
+        ),
+      ],
+    );
+  }
+
+  Widget _switchTorchToggle() => SizedBox(
+        height: 50.0,
+        width: 50.0,
+        child: FloatingActionButton(
+          heroTag: Object(),
+          onPressed: () {
+            setState(() {
+              isTorchOn = !isTorchOn;
+            });
+          },
+          backgroundColor: Color.fromRGBO(183, 193, 250, 1),
+          foregroundColor: Color.fromRGBO(56, 47, 115, 1),
+          child: SvgPicture.asset(
+            "assets/images/Light.svg",
+            width: 34,
+            height: 34,
+            fit: BoxFit.contain,
+          ),
+        ),
+      );
+
+  Future<void> _onDataScanned(String data) async {
+    // logic here :(
+    switch (currentState) {
+      case states.SCANNING_NAME:
+        _scanName(data);
+        break;
+      case states.SCANNING_EAN:
+        _scanEan(data);
+        break;
+      case states.SCANNING_NUTRITION:
+        _scanNutrition(data);
+        break;
+      case states.SCANNING_INGREDIENTS:
+        _scanIngredient(data);
+        break;
+      case states.SCANNING_ALLERGENS:
+        _scanAllergens(data);
+        break;
+      default:
+        break;
+    }
+    return;
+  }
+
+  Future<void> _scanName(String data) async {}
+
+  Future<void> _scanEan(String data) async {}
+  Future<void> _scanNutrition(String data) async {}
+  Future<void> _scanIngredient(String data) async {}
+
+  Future<void> _scanAllergens(String data) async {}
+}
+
+// clipping the center of camera view out
+class HoleClipper extends CustomClipper<Path> {
+  HoleClipper(this.width, this.height);
+  final double width;
+  final double height;
+
+  @override
+  Path getClip(Size size) {
+    var outerPath = Path()
+      ..addRect(Rect.fromLTWH(0.0, 0.0, size.width, size.height));
+
+    var innerPath = Path()
+      ..addRect(Rect.fromCenter(
+        center: Offset(size.width / 2, size.height / 2),
+        width: size.width * width,
+        height: size.width * height,
+      ));
+
+    return Path.combine(PathOperation.difference, outerPath, innerPath);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class _ScannerPage extends StatefulWidget {
+  const _ScannerPage({
+    Key? key,
+    required this.onEanScanned,
+    required this.scanningType,
+    this.isTorchOn = false,
+  }) : super(key: key);
+
+  final Function(String) onEanScanned;
+  final bool isTorchOn;
+  final int scanningType;
+
+  @override
+  _ScannerPageState createState() => _ScannerPageState();
+}
+
+class _ScannerPageState extends State<_ScannerPage> {
+  CustomPaint? _customPaint;
+  var _cameraLensDirection = CameraLensDirection.back;
+  final TextRecognizer _textRecognizer = TextRecognizer();
+  final _barcodeScanner = BarcodeScanner();
+
+  int _timesDidntFoundEan = 0;
+  int _timesFoundEan = 0;
+  bool _canProcess = true;
+  bool _isBusy = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      customPaint: _customPaint,
+      onImage: _processImage,
+      initialCameraLensDirection: _cameraLensDirection,
+      onCameraLensDirectionChanged: (value) => _cameraLensDirection = value,
+      isTorchOn: widget.isTorchOn,
+    );
+  }
+
+  @override
+  void dispose() {
+    _textRecognizer.close();
+    _canProcess = false;
+    super.dispose();
+  }
+
+  Future<void> _processImage(InputImage inputImage) async {
+    // EAN is 1
+    switch (widget.scanningType) {
+      case 0:
+        _getTextFromImage(inputImage);
+        break;
+      case 1:
+        _getEanFromImage(inputImage);
+        break;
+      default:
+        print("Error vole");
+        return;
+    }
+  }
+
+  // cant process image here because it is possible to have 2 states(ean and image)
+  // splitting it into 2 methods with state
+  Future<void> _getTextFromImage(InputImage inputImage) async {
+    if (!_canProcess) {
+      return;
+    }
+    if (_isBusy) return;
+    _isBusy = true;
+    // use this later for displaying a message to aim camera to barcode
+    setState(() {});
+
+    final original = img.decodeImage(inputImage.bytes!);
+
+    if (original == null) {
+      return; // or handle this case appropriately
+    }
+
+    // Define the coordinates and size of the crop region
+    int x = (original.width * 0.25).toInt();
+    int y = (original.height * 0.25).toInt();
+    int w = (original.width * 0.5).toInt();
+    int h = (original.height * 0.5).toInt();
+
+    // Crop the image
+    img.Image cropped = img.copyCrop(original, x: x, y: y, width: w, height: h);
+
+    inputImage = InputImage.fromBytes(
+      bytes: img.encodeJpg(cropped),
+      metadata: InputImageMetadata(
+        size: Size(cropped.width.toDouble(), cropped.height.toDouble()),
+        rotation: InputImageRotation.rotation0deg,
+        format: InputImageFormat.bgra8888,
+        bytesPerRow: cropped.width * 4,
+      ),
+    );
+
+    final recognizedText = await _textRecognizer.processImage(inputImage);
+    print(recognizedText.text);
+
+    _isBusy = false;
+
+    if (mounted) {
+      // display message after some unsucessfull scans to aim camera to barcode
+      setState(() {});
+    }
+  }
+
+  // scan ean from bar_code_scanner later
+  Future<void> _getEanFromImage(InputImage inputImage) async {
+    if (!_canProcess) {
+      return;
+    }
+    if (_isBusy) return;
+    _isBusy = true;
+    // use this later for displaying a message to aim camera to barcode
+    setState(() {});
+    final barcodes = await _barcodeScanner.processImage(inputImage);
+    bool foundEan = barcodes.where((b) => b.displayValue != null).isNotEmpty;
+
+    _isBusy = false;
+
+    if (mounted) {
+      // display message after some unsucessfull scans to aim camera to barcode
+      setState(() {
+        if (foundEan) {
+          _timesDidntFoundEan = 0;
+          _timesFoundEan++;
+          // choose color based on food safety
+          if (_timesFoundEan > 5) {
+            _timesFoundEan = 0;
+            widget.onEanScanned(barcodes.first.displayValue!);
+          }
+          widget.onEanScanned(barcodes.first.displayValue!);
+        } else if (!foundEan && _timesDidntFoundEan > 60) {
+          widget.onEanScanned('');
+          _timesFoundEan = 0;
+        } else {
+          _timesDidntFoundEan++;
+        }
+      });
+    }
   }
 }
